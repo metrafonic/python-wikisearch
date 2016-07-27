cf97e984543bb8c7b1233edd79497b6fc5811a6a
Mathias Hedberg <hedberg.mathias@gmail.com>
2016-07-27 14:53:36 +0200

    changed port binding

--- app.py
+++ app.py
@@ -14,4 +14,7 @@ def create_task():
     #return redirect("http://google.com", code=302)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run( 
+        host="0.0.0.0",
+        port=int("5000")
+  )
